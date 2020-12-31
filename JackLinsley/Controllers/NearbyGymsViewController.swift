94fe6726b68dfb58d1086e2e90a690227a7621b9
Cora-Jane Meade <corajanemeade@gmail.com>
2020-12-31 18:27:39 +0000

    Add NearbyVC and create Gym struct/object

---
+++ JackLinsley/Controllers/NearbyGymsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NearbyGymsViewController.swift
+//  JackLinsley
+//
+//  Created by My Apps on 31/12/2020.
+//
+
+import UIKit
+
+class NearbyGymsViewController : UITableViewController {
+    
+    let gymArray = [
+        Gym(name: "JD Gym", rating: 4, openingHours: true),
+        Gym(name: "Pulse Gym", rating: 3, openingHours: true),
+        Gym(name: "Fitness Gym", rating: 5, openingHours: false),
+    ]
+    
+    override func viewDidLoad() {
+         //let nearestGym = gymArray[0].name
+          
+    }
+    
+    
+}
---
+++ JackLinsley/Models/Gym.swift
@@ -0,0 +1,14 @@
+//
+//  Gym.swift
+//  JackLinsley
+//
+//  Created by My Apps on 31/12/2020.
+//
+
+import Foundation
+
+struct Gym {
+    let name: String
+    let rating: Int
+    let openingHours: Bool
+}
