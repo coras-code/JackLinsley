14cda56eb407305df189dc5d2aff82e754b40840
Cora-Jane Meade <corajanemeade@gmail.com>
2021-01-04 19:56:38 +0000

    Add tableview delegate and datasource methods, add star image assets

--- JackLinsley/Controllers/NearbyGymsViewController.swift
+++ JackLinsley/Controllers/NearbyGymsViewController.swift
@@ -33,5 +33,87 @@ class NearbyGymsViewController : UITableViewController {
           
     }
     
+    //Mark: - Tableview Datasource Methods
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gymArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GymCell", for: indexPath)
+        
+        //create a new cell subclass
+        let nameLabel = cell.viewWithTag(1000) as! UILabel //should it be a var
+        let openingHoursLabel = cell.viewWithTag(100) as! UILabel
+        let ratingPosition1 = cell.viewWithTag(1) as! UIImageView
+        let ratingPosition2 = cell.viewWithTag(2) as! UIImageView
+        let ratingPosition3 = cell.viewWithTag(3) as! UIImageView
+        let ratingPosition4 = cell.viewWithTag(4) as! UIImageView
+        let ratingPosition5 = cell.viewWithTag(5) as! UIImageView
+        
+        
+        nameLabel.text = gymArray[indexPath.row].name
+        
+        if gymArray[indexPath.row].openingHours == true {
+            openingHoursLabel.text = "Open Now"
+        } else {
+            openingHoursLabel.text = "Closed Now"
+        }
+        //how to refactor this
+        switch gymArray[indexPath.row].rating {
+        case 1:
+            ratingPosition1.image = UIImage(named: "star_icon_filled")
+            ratingPosition2.image = UIImage(named: "star_icon")
+            ratingPosition3.image = UIImage(named: "star_icon")
+            ratingPosition4.image = UIImage(named: "star_icon")
+            ratingPosition5.image = UIImage(named: "star_icon")
+        case 2:
+            ratingPosition1.image = UIImage(named: "star_icon_filled")
+            ratingPosition2.image = UIImage(named: "star_icon_filled")
+            ratingPosition3.image = UIImage(named: "star_icon")
+            ratingPosition4.image = UIImage(named: "star_icon")
+            ratingPosition5.image = UIImage(named: "star_icon")
+        case 3:
+            ratingPosition1.image = UIImage(named: "star_icon_filled")
+            ratingPosition2.image = UIImage(named: "star_icon_filled")
+            ratingPosition3.image = UIImage(named: "star_icon_filled")
+            ratingPosition4.image = UIImage(named: "star_icon")
+            ratingPosition5.image = UIImage(named: "star_icon")
+        case 4:
+            ratingPosition1.image = UIImage(named: "star_icon_filled")
+            ratingPosition2.image = UIImage(named: "star_icon_filled")
+            ratingPosition3.image = UIImage(named: "star_icon_filled")
+            ratingPosition4.image = UIImage(named: "star_icon_filled")
+            ratingPosition4.image = UIImage(named: "star_icon")
+        case 5:
+            ratingPosition1.image = UIImage(named: "star_icon_filled")
+            ratingPosition2.image = UIImage(named: "star_icon_filled")
+            ratingPosition3.image = UIImage(named: "star_icon_filled")
+            ratingPosition4.image = UIImage(named: "star_icon_filled")
+            ratingPosition5.image = UIImage(named: "star_icon_filled")
+        
+        default: //can i get rid as i wont need this if the rating is always 1-5
+            ratingPosition1.image = UIImage(named: "star_icon")
+            ratingPosition2.image = UIImage(named: "star_icon")
+            ratingPosition3.image = UIImage(named: "star_icon")
+            ratingPosition4.image = UIImage(named: "star_icon")
+            ratingPosition5.image = UIImage(named: "star_icon")
+        }
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+      //Mark: - Tableview Delegate Methods
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+       tableView.allowsSelection = false
+       
+    }
+
 }
+
+
+
+
