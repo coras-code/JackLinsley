dfdeb96f09d0f56d8e59ddd46e13d0edb448fb3b
Cora-Jane Meade <corajanemeade@gmail.com>
2021-01-06 22:24:48 +0000

    Refactor code

--- JackLinsley/Controllers/NearbyGymsViewController.swift
+++ JackLinsley/Controllers/NearbyGymsViewController.swift
@@ -11,9 +11,8 @@ class NearbyGymsViewController : UITableViewController {
     
     var coordinateMode = true
     
-    //need a strings class
-    
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var barButton: UIBarButtonItem!
     
     let gyms = [
         Gym(name: "JD Gym", rating: 2, openingHours: true),
@@ -36,15 +35,10 @@ class NearbyGymsViewController : UITableViewController {
     
     override func viewDidLoad() {
         searchBar.delegate = self
-        //if coordinateMode {
-       //     searchBar.isUserInteractionEnabled = false
-     
-            
-      //  }
-          
+        coordinateModeEnabled()
     }
     
-    //Mark: - Tableview Datasource Methods
+    //MARK: - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return gyms.count
     }
@@ -55,8 +49,8 @@ class NearbyGymsViewController : UITableViewController {
         
         cell.selectionStyle = .none
         
-       // Linking cell in view to controller - create a new CELL subclass
-        let nameLabel = cell.viewWithTag(1000) as! UILabel //should it be a var
+        // Linking cell in view to controller - create a new CELL subclass
+        let nameLabel = cell.viewWithTag(1000) as! UILabel
         let openingHoursLabel = cell.viewWithTag(100) as! UILabel
         let imagePosition1 = cell.viewWithTag(1) as! UIImageView
         let imagePosition2 = cell.viewWithTag(2) as! UIImageView
@@ -75,58 +69,56 @@ class NearbyGymsViewController : UITableViewController {
         for (index, position) in ratingPositions.enumerated() {
             position.image = index < rating ? UIImage(named: "star_icon_filled") : UIImage(named: "star_icon")
         }
-    
+        
         return cell
     }
     
-    //Mark: - Tableview Delegate Methods
+    //MARK: - Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-       tableView.allowsSelection = false
-       
+        tableView.allowsSelection = false
+        
     }
     
     //MARK: - Bar Button Item
-    @IBOutlet weak var barButton: UIBarButtonItem!
-    
-    
     @IBAction func barButtonPressed(_ sender: UIBarButtonItem) {
         //this changes the mode
         if coordinateMode {
+            //addressModeEnabled:
             sender.image =  UIImage(systemName: "pencil.circle")
-            searchBar.isUserInteractionEnabled = true
-            //change input etc. //now it is in address mode
+            searchBar.placeholder = "Search using address"
         } else {
-            sender.image =  UIImage(systemName: "globe")
-            //now it is coordinate mode, input is lat and longitiude
+            coordinateModeEnabled()
         }
-        
         coordinateMode = !coordinateMode
-
+        searchBar.text = ""// clear the search bar
+        //clear table view?
+        //replace tableview with current location date, to show this have a filled location
     }
     
-    //SearchBar
-    
-   
-    
+    func coordinateModeEnabled() {
+        barButton.image =  UIImage(systemName: "globe")
+        searchBar.placeholder = "Search using latitude and longitude"
+        
+    }
 }
 
 //MARK: - Search Bar Methods
 extension NearbyGymsViewController: UISearchBarDelegate {
-    //let coordinates = ""
+    //let coordinates = "" // for API
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         if coordinateMode {
-            showAlert()
+            coordinateEntry()
+            searchBar.isUserInteractionEnabled = false //do i need this line, because i dont think i disable it
             return false
         } else {
+            searchBar.isUserInteractionEnabled = true //dont think i need this
             return true
         }
-        
     }
     
-    
-    func showAlert() {
+    func coordinateEntry() {
         
         var latitudeTextField = UITextField()
         var longitudeTextField = UITextField()
@@ -134,9 +126,14 @@ extension NearbyGymsViewController: UISearchBarDelegate {
         let alert = UIAlertController(title: "Find nearby gyms to a specfic location", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Search Gyms", style: .default) { (action) in
-            //what will happen when you press action button
-            //dont force on wrap?
-            self.searchBar.searchTextField.text = "\(latitudeTextField.text!); \(longitudeTextField.text!)"
+            //Result of action button pressed
+            if latitudeTextField.text != "", longitudeTextField.text != "" {
+                self.searchBar.searchTextField.text = "\(latitudeTextField.text!); \(longitudeTextField.text!)"
+            }
+            
+            //can force unwrap because a textfield can nevr be nil - just an empty string
+           //need to deal with empty strings: one and other
+            //loading cell appears //then tabledata reloads with new info
         }
         
         alert.addTextField { (alertTextField) in
@@ -150,35 +147,25 @@ extension NearbyGymsViewController: UISearchBarDelegate {
         }
         
         alert.addAction(action)
-         
         present(alert, animated: true, completion: nil)
+        
+        //latitudeTextField.text! //longitudeTextField.text! - this needs to be passed in as an input
     }
-        
-//        searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-//         let alert = UIAlertController(title: "jjj", message: "",u preferredStyle: .alert)
-//               let action = UIAlertAction(title: "Search Gyms", style: .default) { (action) in
-//                  //what will happen when you press add item
-//                   print("Print Success")
-//              }
-//
-//              alert.addAction(action)
-//
-//               present(alert, animated: true, completion: nil)
-//
-        
-        //put the inputs into the search box
-       // searchBarSearchButtonClicked(searchBar)
-        
-//    }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-       
-       
+        //this only comes up in address mode
     }
-    
-    
-    
 }
+
+
+
+
+
+
+
+
+
+
 
 //alternate for the cel? delete if dont use?
 class GymCell: UITableViewCell {
